821624127cafdb154d28ef786b11afdf2afa5a57
박연우 <parkyeonwoo@bag-yeon-uui-MacBookAir.local>
2024-07-28 21:27:14 +0900

    [Feat] User 모델에 닉네임, 라이프스타일 필드 추가

--- accounts/models.py
+++ accounts/models.py
@@ -4,9 +4,11 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    nickname = models.CharField(verbose_name="닉네임", max_length=10, blank=True)
     city = models.CharField(verbose_name='시', max_length=20, blank=True)
     gu = models.CharField(verbose_name='구', max_length=20, blank=True)
     goon = models.CharField(verbose_name='군', max_length=20, blank=True)
+    lifestyle = models.CharField(verbose_name="라이프스타일", max_length=30, blank=True)
 
     @staticmethod
     def get_user_or_none_by_username(username): # username값으로 해당 유저를 찾는 모델 내부 함수 
