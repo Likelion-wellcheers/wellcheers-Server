2a63ac8d7bcb80194b6f8a4e5870df49a3b6fb5c
박연우 <parkyeonwoo@bag-yeon-uui-MacBookAir.local>
2024-07-29 22:10:48 +0900

    [Feat] 사용자 토큰으로 식별하는 모델 내부 함수 구현

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from rest_framework_simplejwt.tokens import UntypedToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 # Create your models here.
 class User(AbstractUser):
@@ -22,4 +24,29 @@ class User(AbstractUser):
         try:
             return User.objects.get(email=email)
         except Exception:
+            return None
+    
+    
+    @staticmethod
+    def get_user_or_none_by_token(token): # token값으로 해당 유저를 찾는 모델 내부 함수
+        # jwt 토큰 디코딩
+        try:
+            # UntypedToken을 사용하여 토큰을 디코딩
+            untoken = UntypedToken(token)
+
+            # 토큰이 유효하면 데이터(payload)를 저장
+            payload = untoken.payload
+            if not payload: return None
+
+            user_id = payload.get('user_id') # 토큰에서 유저 이메일 값을 가져옴
+            print(payload)
+            if not user_id:
+                print('해당 토큰을 가진 유저가 없습니다.')
+                return None
+            
+            return User.objects.get(id=user_id)
+
+        except (InvalidToken, TokenError) as e:
+            # 토큰이 유효하지 않으면 예외 처리
+            print(f"토큰이 유효하지 않습니다.: {e}")
             return None
