931d554a9987b9f4035da43862f43253f0cd0bdf
박연우 <parkyeonwoo@bag-yeon-uui-MacBookAir.local>
2024-07-31 19:12:09 +0900

    [Refactor] 해당 지역 질문글 보기 기능 url 수정

--- qna/urls.py
+++ qna/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from qna.views import QnA, QuestionList
+from qna.views import QnA, QuestionList, QuestionDetail
 
 urlpatterns = [
     path("", QnA.as_view()),
-    path("<int:region_id>/question/", QuestionList.as_view()),
+    path("<int:region_id>/", QuestionList.as_view()),
 ]
