e8746b024de5f3adc6a22772c8ad9721165b4ab7
박연우 <parkyeonwoo@bag-yeon-uui-MacBookAir.local>
2024-08-02 23:28:02 +0900

    [Fix] 특정 시 입력하면 citycode 반환하는 기능 오류 수정

--- post/urls.py
+++ post/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import AtriclePost, ChoiceRegion, WellfareByRegionView,MagazineByRegionView, Regionreview, Regionreviewlook,WellOne,MagOne
 
 urlpatterns = [
-    path("<int:id>/", ChoiceRegion.as_view(), name='choiceregion.군구보여줌'),
+    path("region/", ChoiceRegion.as_view(), name='choiceregion.군구보여줌'),
     path("<int:id>/getmagazine/", MagazineByRegionView.as_view(), name='region으로매거진리스트업'),
     path("<int:id>/welfare/", WellfareByRegionView.as_view(), name="region으로 복지정책 리스트업"),
     path("welfare/<int:id>/",WellOne.as_view(), name="정책개별보기"),
