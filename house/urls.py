47272753bb6396c0a2eb2b08243ecc42ac1450be
yebinnnnn <prettybird1@naver.com>
2024-08-02 23:32:53 +0900

    [Fix]기타 api 오류수정

--- house/urls.py
+++ house/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CenterReviewLookView, Recommend, RegionInformation, CenterReviewView, CenterList, CenterView, MyCart, MyReport, ReportWrite
+from .views import CenterReviewLookView, Recommend, RegionInformation, CenterReviewView, CenterList, CenterView, MyCart, MyReport, MyCartInsert, ReportWrite
 
 urlpatterns = [
     path("", Recommend.as_view(), name='recommend'),
@@ -8,8 +8,8 @@ urlpatterns = [
     path("center/<int:id>/", CenterView.as_view(), name="CenterView"),
     path("center/<int:id>/review/", CenterReviewView.as_view(), name="CenterReviewView"),
     path("center/review/<int:id>/", CenterReviewLookView.as_view(), name="CenterReviewLookView"),
-    path("mycart/", MyCart.as_view(), name="cartView"),
+    path("mycart/", MyCartInsert.as_view(), name="cartView"),
     path("mycart/<int:id>/",MyCart.as_view(), name="cartPut"),
-    path("mycart/<int:id>/",MyReport.as_view(), name="ReportView"),
+    path("mycart/<int:id>/budget/",MyReport.as_view(), name="ReportView"),
     path("mycart/report/",ReportWrite.as_view(),name="ReportWrite")
 ]
