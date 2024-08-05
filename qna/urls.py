922b6c6b00aaeecceee855dea78d445d38452e91
박연우 <parkyeonwoo@bag-yeon-uui-MacBookAir.local>
2024-08-05 21:01:56 +0900

    [Feat] qna 해당 질문에 달린 답변 리스트업 기능

--- qna/urls.py
+++ qna/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from qna.views import QnA, QuestionDetail, MyQuestion, MyAnswer, QuestionList
+from qna.views import AnswerList, QnA, QuestionDetail, MyQuestion, MyAnswer, QuestionList
 
 urlpatterns = [
     path("", QnA.as_view()),
     path("question/", QuestionList.as_view()),
     path("question/<int:q_id>/", QuestionDetail.as_view()),
+    path("question/<int:q_id>/answer/", AnswerList.as_view()),
     path("myquestion/", MyQuestion.as_view()),
     path("myanswer/", MyAnswer.as_view()),
 ]
